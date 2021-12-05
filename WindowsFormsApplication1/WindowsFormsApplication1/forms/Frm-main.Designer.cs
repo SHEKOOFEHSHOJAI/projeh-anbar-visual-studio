d51edd9ede544490662898bfe868751635a2848d
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2021-12-05 12:29:47 -0800

    comi3

--- WindowsFormsApplication1/WindowsFormsApplication1/forms/Frm-main.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/forms/Frm-main.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.advPropertyGrid1 = new DevComponents.DotNetBar.AdvPropertyGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.advPropertyGrid1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // advPropertyGrid1
+            // 
+            this.advPropertyGrid1.GridLinesColor = System.Drawing.Color.WhiteSmoke;
+            this.advPropertyGrid1.Location = new System.Drawing.Point(644, 118);
+            this.advPropertyGrid1.Name = "advPropertyGrid1";
+            this.advPropertyGrid1.Size = new System.Drawing.Size(46, 31);
+            this.advPropertyGrid1.TabIndex = 0;
+            this.advPropertyGrid1.Text = "advPropertyGrid1";
             // 
             // انبارداری
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 725);
+            this.Controls.Add(this.advPropertyGrid1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "انبارداری";
@@ -42,10 +54,13 @@
             this.RightToLeftLayout = true;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "انبارداری";
+            ((System.ComponentModel.ISupportInitialize)(this.advPropertyGrid1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.AdvPropertyGrid advPropertyGrid1;
     }
 }
